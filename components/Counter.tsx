27032f8cd5e726c8a4aa4ee99b8c3f3210b173e7
Shiv Bhonde <shivbhonde04@gmail.com>
2023-10-10 22:47:35 +0530

    add basic two useEffect example

---
+++ components/Counter.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useEffect, useState } from "react";
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("First useEffect runs when count update");
+  }, [count]);
+
+  useEffect(() => {
+    console.log("Second useEffect");
+    setCount((prev) => prev + 1);
+  }, []);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  const handleReset = () => {
+    setCount(0);
+  };
+
+  return (
+    <>
+      <h1>You have clicked the button {count} times</h1>
+      <button onClick={handleClick}>Click me!</button>
+      <button onClick={handleReset}>Reset</button>
+    </>
+  );
+}
